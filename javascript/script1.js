c144dc21c8a5267c183c922fd2f8a07662afd327
jhonatanccsouza <jhonatan.github@gmail.com>
2025-06-11 12:34:24 -0300

    atualização media-queries

---
+++ javascript/script1.js
@@ -0,0 +1,29 @@
+ function monitorTamanho(){
+    if(window.innerWidth >= 768) {
+
+        document.body.header.style.position = 'relative'
+        
+    }
+}
+function clickMenu(){
+    
+
+    if(burguermenu.style.display == 'block'){
+        burguermenu.style.display = 'none'
+    } else {
+        burguermenu.style.display = 'block'
+    }
+
+    
+}
+
+function mousedentro(){
+    if (burguermenu.style.display == 'none'){
+        burguermenu.style.display = 'block'
+    } 
+}
+function mousesai(){
+    if (burguermenu.style.display == 'block'){
+        burguermenu.style.display = 'none'
+    }
+}
